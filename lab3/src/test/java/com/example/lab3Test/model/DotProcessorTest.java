c905b79ba2371def743e6b0c38c45913d2057971
misuy <mperedriy@bk.ru>
2023-05-17 16:16:18 +0300

    tests

--- lab3/src/test/java/com/example/lab3Test/model/DotProcessorTest.java
+++ lab3/src/test/java/com/example/lab3Test/model/DotProcessorTest.java
@@ -8,14 +8,44 @@ import org.junit.jupiter.api.Test;
 public class DotProcessorTest {
 
     @Test
-    void testDotProcessorSuccess() {
+    void testDotProcessorSegment1Success() {
         Dot dot = new Dot(1.0, 1.0, 3.0);
         Assertions.assertTrue(DotProcessor.processDot(dot));
     }
 
     @Test
-    void testDotProcessorFail() {
+    void testDotProcessorSegment1Fail() {
         Dot dot = new Dot(2.0, 2.0, 3.99);
+        Assertions.assertFalse(DotProcessor.processDot(dot));
+    }
+
+    @Test
+    void testDotProcessorSegment2Fail() {
+        Dot dot = new Dot(-0.001, 0.001, 1000);
+        Assertions.assertFalse(DotProcessor.processDot(dot));
+    }
+
+    @Test
+    void testDotProcessorSegment3Success() {
+        Dot dot = new Dot(-0.1, -1, 3);
+        Assertions.assertTrue(DotProcessor.processDot(dot));
+    }
+
+    @Test
+    void testDotProcessorSegment3Fail() {
+        Dot dot = new Dot(-1, -2, 3);
+        Assertions.assertFalse(DotProcessor.processDot(dot));
+    }
+
+    @Test
+    void testDotProcessorSegment4Success() {
+        Dot dot = new Dot(3, -0.99, 8);
+        Assertions.assertTrue(DotProcessor.processDot(dot));
+    }
+
+    @Test
+    void testDotProcessorSegment4Fail() {
+        Dot dot = new Dot(3, -1.01, 8);
         Assertions.assertFalse(DotProcessor.processDot(dot));
     }
 }
